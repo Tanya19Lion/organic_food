a67e76c3edb2543c11988d12e0cd997848818cdc
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-06-22 22:26:55 +0300

    added timer

--- app/js/main.js
+++ app/js/main.js
@@ -67,3 +67,57 @@ function calcScroll() {
 
     return scrollWidth;
 }
+
+
+//timer
+const getTimeRemaining = (endtime) => {
+    const time = Date.parse(endtime) - Date.parse(new Date()),
+        minutes = Math.floor((time / 1000 / 60) % 60),
+        hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+        days = Math.floor((time / (1000 * 60 *60 *24)));
+    
+    return {
+        'total': time,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes
+    }
+};
+
+const addZero = (num) => {
+    if (num <= 9) {
+        return '0' + num;
+    } else {
+        return num;
+    }
+};
+
+const setClock = (selector, endtime) => {
+    const timer = document.querySelector('.timer__block'),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes');
+        timeInterval = setInterval(updateClock, 60000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.textContent = addZero(t.days);
+        hours.textContent = addZero(t.hours);
+        minutes.textContent = addZero(t.minutes);
+
+        if (t.total <= 0) {
+            days.textContent = '00';
+            hours.textContent = '00';
+            minutes.textContent = '00';
+
+            clearInterval(timeInterval);
+        }
+    }
+};
+
+
+
+setClock('.timer__block', '2020-06-30');
